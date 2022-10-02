6d8f391656802f1e87ca7c06f499ac45ed48d4ba
Jacky <jackyj12@nycstudents.net>
2022-10-02 14:56:39 -0400

    Finished

--- src/Point.java
+++ src/Point.java
@@ -17,5 +17,47 @@ public class Point {
         y = 0;
     }
 
-}
+    public int getX() {
+        return x;
+    }
 
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String coordinate() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "The point is in quadrant 1.";
+        } else {
+            if (x < 0 && y > 0) {
+                return "The point is in quadrant 2.";
+            } else {
+                if (x < 0 && y < 0) {
+                    return "The point is in quadrant 3.";
+                } else {
+                    if (x > 0 && y < 0) {
+                        return "The point is in quadrant 4.";
+                    } else {
+                        if (x == 0 && y != 0 || x != 0 && y == 0) {
+                            return "The point is on an axis.";
+                        } else {
+                            return "The point is on the origin.";
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
